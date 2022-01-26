4ae24f7556a2245b6d655671da40cc2638e53223
Apelsini <45172369>
2022-01-26 20:35:51 +0300

    r

---
+++ Sachek-Domashka1.py
@@ -0,0 +1,7 @@
+M = 1
+N = 30
+S = 7  # WRN: this Quantity is never used in the task
+L = 3
+tot_price = (M + N / 100) * L
+tot_price_typized = tot_price * 100 // 1 / 100
+print("общая цена в рублях и копейках=", tot_price_typized)
